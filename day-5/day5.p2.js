d088f2a49b8c8e036bcd00d7220bbdb980dc5041
Antonin Gontier <gontier.antonin@orange.fr>
2021-12-06 01:50:51 +0100

    Solve day 5

---
+++ day-5/day5.p2.js
@@ -0,0 +1,58 @@
+const lineReader = require('line-reader');
+
+let ventMap = [...new Array(1000)].map(() => new Array(1000).fill(0));
+let dangerousPoints = 0;
+
+lineReader.eachLine('day5.input.txt', function(line, last) {
+  let match = /(\d+),(\d+)\s+->\s+(\d+),(\d+)/g.exec(line);
+  let x1 = match[1];
+  let y1 = match[2];
+  let x2 = match[3];
+  let y2 = match[4];
+
+  if (x1 === x2) {
+    for (let y = Math.min(y1, y2); y <= Math.max(y1, y2); y++) {
+      ventMap[x1][y]++;
+      if (ventMap[x1][y] === 2) {
+        dangerousPoints++;
+      }
+    }
+  } else if (y1 === y2) {
+    for (let x = Math.min(x1, x2); x <= Math.max(x1, x2); x++) {
+      ventMap[x][y1]++;
+      if (ventMap[x][y1] === 2) {
+        dangerousPoints++;
+      }
+    }
+  } else if ((y2-y1)/(x2-x1) === 1) {
+    let pointToPlace = Math.max(x1,x2)-Math.min(x1,x2)+1;
+    for (let i = 0; i < pointToPlace; i++) {
+      let x = Math.min(x1,x2)+i;
+      let y = Math.min(y1,y2)+i;
+      ventMap[x][y]++;
+      if (ventMap[x][y] === 2) {
+        dangerousPoints++;
+      }
+    }
+  } else if ((y2-y1)/(x2-x1) === -1) {
+    let pointToPlace = Math.max(x1,x2)-Math.min(x1,x2)+1;
+    for (let i = 0; i < pointToPlace; i++) {
+      let x = Math.max(x1,x2)-i;
+      let y = Math.min(y1,y2)+i;
+      ventMap[x][y]++;
+      if (ventMap[x][y] === 2) {
+        dangerousPoints++;
+      }
+    }
+  }
+
+  if (last) {
+    solve();
+  }
+});
+
+function solve() {
+  // console.table(ventMap);
+
+  console.log('Result : ', dangerousPoints);
+}
