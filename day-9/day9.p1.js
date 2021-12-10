29ea30d3773f0707f5f2357d85c0d6cae896c6a2
Antonin Gontier <gontier.antonin@orange.fr>
2021-12-10 01:18:46 +0100

    Add day 9

---
+++ day-9/day9.p1.js
@@ -0,0 +1,42 @@
+const lineReader = require('line-reader');
+
+let heightMap = [];
+
+lineReader.eachLine('day9.input.txt', function(line, last) {
+  heightMap.push(line.split('').map(v => parseInt(v)));
+
+  if (last) {
+    solve();
+  }
+});
+
+function solve() {
+  let res = 0;
+
+  for (let i = 0; i < heightMap.length; i++) {
+    for (let j = 0; j < heightMap[i].length; j++) {
+      if(isLowPoint(i, j)) {
+        res += 1+heightMap[i][j];
+      }
+    }
+  }
+
+  console.log('Result : ', res);
+}
+
+function isLowPoint(x, y) {
+  const currentValue = heightMap[x][y];
+  if (x > 0 && currentValue >= heightMap[x-1][y]) {
+    return false;
+  }
+  if (y > 0 && currentValue >= heightMap[x][y-1]) {
+    return false;
+  }
+  if (x < heightMap.length-1 && currentValue >= heightMap[x+1][y]) {
+    return false;
+  }
+  if (y < heightMap[x].length-1 && currentValue >= heightMap[x][y+1]) {
+    return false;
+  }
+  return true;
+}
---
+++ day-9/day9.p2.js
@@ -0,0 +1,59 @@
+const lineReader = require('line-reader');
+const jsgraphs = require('js-graph-algorithms');
+
+let heightMap = [];
+let xlength;
+let ylength;
+let g;
+
+lineReader.eachLine('day9.input.txt', function(line, last) {
+  heightMap.push(line.split('').map(v => parseInt(v)));
+
+  if (last) {
+    solve();
+  }
+});
+
+function solve() {
+  xlength = heightMap.length;
+  ylength = heightMap[0].length;
+  g = new jsgraphs.Graph(xlength*ylength);
+
+  for (let i = 0; i < heightMap.length; i++) {
+    for (let j = 0; j < heightMap[i].length; j++) {
+      buildGraph(i, j);
+    }
+  }
+
+  //Polluted by 9s
+  let cc = new jsgraphs.ConnectedComponents(g);
+
+  let occ = cc.id.reduce((acc, v) => {
+    acc[v] ??= 0;
+    acc[v]++;
+    return acc;
+  }, {});
+
+  const maxConnex = [...Object.values(occ)].sort((a,b) => a-b).reverse().slice(0, 3);
+
+  console.log('Result : ', maxConnex.reduce((acc, v) => acc *= v, 1));
+}
+
+function buildGraph(x, y) {
+  const currentValue = heightMap[x][y];
+  const currentNode = nodeId(x, y);
+  if (currentValue !== 9) {
+    if (x < heightMap.length-1 && heightMap[x+1][y] !== 9) {
+      const targetNode = nodeId(x+1, y);
+      g.addEdge(currentNode, targetNode);
+    }
+    if (y < heightMap[x].length-1 && heightMap[x][y+1] !== 9) {
+      const targetNode = nodeId(x, y+1);
+      g.addEdge(currentNode, targetNode);
+    }
+  }
+}
+
+function nodeId(x, y) {
+  return (ylength * x) + y;
+}
